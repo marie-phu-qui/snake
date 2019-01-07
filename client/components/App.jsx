ca71f552e87bdd20f49ebf22a9f0658774f31b3a
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-01-07 17:20:26 +1300

    interface full react/webpack 4 is up showing client

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                I am a snake snake
+            </h1>
+        </div>
+    )
+  }
+
+export default App
