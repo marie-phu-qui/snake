0efecef91607ac781f07204a4d4b7a727e0780de
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-01-11 17:05:00 +1300

    set up handlechange on keyboard event for Snake

--- client/components/Snake.jsx
+++ client/components/Snake.jsx
@@ -3,21 +3,28 @@ import { render } from 'react-dom';
 import { Stage, Layer, Rect, Text } from 'react-konva';
 
 class Snake extends React.Component {
-  state = {
-    color: 'green',
-    x : 100,
-    y : 100
-  };
-  handleClick = () => {
-    this.setState({
-      
-    });
-  };
+    // 
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'red',
+            x : 100,
+            y : 100
+        };
+        this.handleChange = this.handleChange.bind(this);
+      }
+    
+      handleChange(e) {
+        this.setState({
+          ...this.state,
+          [e.target.name]: e.target.value
+        });
+      }
   render() {
     return (
         <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-            <Text text="one square sized snake" />
+            <Text text="one square sized snake - moves on keyboard input" />
             <Rect
                 x={this.state.x}
                 y={this.state.y}
