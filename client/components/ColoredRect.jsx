fbed0720103083f2c0b03c61af67c0a3effe2759
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-01-11 15:57:08 +1300

    testing server ends

---
+++ client/components/ColoredRect.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+
+// Get to know Konva exemple
+class ColoredRect extends React.Component {
+  state = {
+    color: 'green'
+  };
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+  render() {
+    return (
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Click on the square to change color" />
+          <Rect
+            x={20}
+            y={20}
+            width={50}
+            height={50}
+            fill={this.state.color}
+            shadowBlur={5}
+            onClick={this.handleClick}
+            />
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+export default ColoredRect
